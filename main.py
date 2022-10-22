82a2d9333f69fdf3b3f77846287620447ebed2c5
João Pedro <>
2022-10-22 20:32:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#wallet = 41
+
+#wallet = 32
+
+#day = 22
+#print(day+3)
+
+Age = 50
+Height = 1.91
+
+shirt = 'Blue'
+Store = "Nick's Pizza Shop"
+
+Film = "City of Angels"
+
+day = 22
+month = 'October'
+temp = 65
+
+print("hoje é dia ", day, 'de ', month, ' e estao ', temp, ' graus')
+
+print(f"Hoje é {month} {day} e estão {temp} graus lá fora")
+
+day_name = "Saturday"
+
+print(f"Hoje é {month} {day} {day_name} e estão {temp} graus lá fora")
